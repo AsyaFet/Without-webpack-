39addd94a207bb29bcb69f0a306c6eb1ee74741d
Asya Fetisova <aanastai.logos@gmail.com>
2020-05-08 13:28:20 +0300

    array

--- scripts/operators.js
+++ scripts/operators.js
@@ -1,55 +1,41 @@
 const age = 17;
 
 // 1) Проверьте переменную age и если возраст больше или равен 18 и меньше или равен 21, выведите в консоль соответсвующее сообщение
-age >= 18 && age <= 21
-  ? console.log("Your age is appropriate")
-  : console.log("Your age isn`t aprropriate");
 
 const users = [
   {
     name: "Roman",
     age: 28,
     visitedPark: true,
-    boughtItems: false,
+    boughtItems: false
   },
   {
     name: "Maya",
     age: 14,
     visitedPark: false,
-    boughtItems: true,
+    boughtItems: true
   },
   {
     name: "Alex",
     age: 45,
     visitedPark: false,
-    boughtItems: false,
+    boughtItems: false
   },
   {
     name: "Tonia",
     age: 32,
     visitedPark: true,
-    boughtItems: true,
-  },
+    boughtItems: true
+  }
 ];
 
 // 2) Для каждого элемента массива users выведите сообщение в консоль, только если пользователь посетил парк или совершил покупку
 // Используйте foreach или for, а потом if внутри петли
 
-users.forEach((user) => {
-  visitedPark == true || boughtItems == true
-    ? console.log("You have come actions", user)
-    : console.log("You haven`t apropriate actions", user);
-});
-
 let iterationCounter = "Each iteration adds a *: ";
 
-for (let i = 0; i < users.length; i++) {
-  iterationCounter += " * ";
-  parseInt(iterationCounter, i);
-  console.log(user[i]);
+for (let i = 0; i < 10; i++) {
+  console.log(i);
   // 3) Используя assigment operator(+=) поменяйте значение переменоой iterationCounter, добавляя по одной звездочке при каждой итерации
 }
-
-console.log(iterationCounter);
-
 // 4) Выведите в консоль значение iterationCounter. Ожидаемый результат "Each iteration adds a *: * * * * * * * * * *"
